13d910cb1de7d8242ed6d6afceeb2c72030eae9e
rafaelreisramos <rrramos@gmail.com>
2023-06-28 13:04:50 -0300

    fix: add missing null type on `activeCycleId`

--- projeto-02__ignite-timer/src/reducers/cycles/reducer.ts
+++ projeto-02__ignite-timer/src/reducers/cycles/reducer.ts
@@ -11,7 +11,7 @@ export interface Cycle {
 
 interface CyclesState {
   cycles: Cycle[]
-  activeCycleId: string
+  activeCycleId: string | null
 }
 
 export function cyclesReducer(state: CyclesState, action: any) {

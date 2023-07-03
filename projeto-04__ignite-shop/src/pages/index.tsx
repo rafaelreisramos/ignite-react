ba47d9f04f90c4235320d6b5378938edd9d3ceb4
rafaelreisramos <rrramos@gmail.com>
2023-07-03 18:45:58 -0300

    feat: add carousel with `keen-slider`

--- projeto-04__ignite-shop/src/pages/index.tsx
+++ projeto-04__ignite-shop/src/pages/index.tsx
@@ -1,24 +1,51 @@
 import Image from 'next/image'
 import { HomeContainer, ProductCard } from '../styles/pages/home'
+import { useKeenSlider } from 'keen-slider/react'
+import 'keen-slider/keen-slider.min.css'
 
 import igniteLab from '../assets/shirts/ignite-lab.png'
 import igniterAbord from '../assets/shirts/igniter-abord.png'
+import maratonaExplorer from '../assets/shirts/maratona-explorer.png'
 
 export default function Home() {
+  const [sliderRef] = useKeenSlider({
+    slides: {
+      perView: 3,
+      spacing: 48,
+    },
+  })
+
   return (
-    <HomeContainer>
-      <ProductCard href="#">
+    <HomeContainer ref={sliderRef} className="keen-slider">
+      <ProductCard href="#" className="keen-slider__slide">
         <Image src={igniteLab} width={520} height={480} alt="" />
         <footer>
           <strong>Camiseta Ignite Lab</strong>
           <span>R$ 89,99</span>
         </footer>
       </ProductCard>
-      <ProductCard href="#">
+
+      <ProductCard href="#" className="keen-slider__slide">
         <Image src={igniterAbord} width={520} height={480} alt="" />
         <footer>
           <strong>Camiseta Igniter Aboard</strong>
           <span>R$ 79,99</span>
+        </footer>
+      </ProductCard>
+
+      <ProductCard href="#" className="keen-slider__slide">
+        <Image src={maratonaExplorer} width={520} height={480} alt="" />
+        <footer>
+          <strong>Camiseta Maratona Explorer</strong>
+          <span>R$ 69,99</span>
+        </footer>
+      </ProductCard>
+
+      <ProductCard href="#" className="keen-slider__slide">
+        <Image src={maratonaExplorer} width={520} height={480} alt="" />
+        <footer>
+          <strong>Camiseta Maratona Explorer</strong>
+          <span>R$ 69,99</span>
         </footer>
       </ProductCard>
     </HomeContainer>
--- projeto-04__ignite-shop/src/styles/pages/home.ts
+++ projeto-04__ignite-shop/src/styles/pages/home.ts
@@ -2,8 +2,8 @@ import { styled } from '..'
 
 export const HomeContainer = styled('main', {
   display: 'flex',
-  gap: '3rem',
-  width: 'min(100%, calc(100vw - ((100vw - 1180px) / 2)))',
+  width: '100%',
+  maxWidth: 'calc(100vw - ((100vw - 1180px) / 2))',
   marginLeft: 'auto',
   minHeight: 656,
 })

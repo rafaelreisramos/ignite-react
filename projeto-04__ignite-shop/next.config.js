bb286890bd52ab3e3811fd752e8c5a104b348825
rafaelreisramos <rrramos@gmail.com>
2023-07-03 21:28:50 -0300

    feat: add `stripe` and `ssr`

--- projeto-04__ignite-shop/next.config.js
+++ projeto-04__ignite-shop/next.config.js
@@ -2,6 +2,14 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'files.stripe.com',
+      },
+    ],
+  },
 }
 
 module.exports = nextConfig
--- projeto-04__ignite-shop/src/pages/index.tsx
+++ projeto-04__ignite-shop/src/pages/index.tsx
@@ -1,13 +1,25 @@
 import Image from 'next/image'
-import { HomeContainer, ProductCard } from '../styles/pages/home'
+import { GetServerSideProps } from 'next'
+import Stripe from 'stripe'
+import { stripe } from '../lib/stripe'
 import { useKeenSlider } from 'keen-slider/react'
 import 'keen-slider/keen-slider.min.css'
 
-import igniteLab from '../assets/shirts/ignite-lab.png'
-import igniterAbord from '../assets/shirts/igniter-abord.png'
-import maratonaExplorer from '../assets/shirts/maratona-explorer.png'
+import { HomeContainer, ProductCard } from '../styles/pages/home'
 
-export default function Home() {
+interface Product {
+  id: string
+  name: string
+  description: string
+  imageUrl: string
+  price: number
+}
+
+interface HomeProps {
+  products: Product[]
+}
+
+export default function Home({ products }: HomeProps) {
   const [sliderRef] = useKeenSlider({
     slides: {
       perView: 3,
@@ -17,37 +29,41 @@ export default function Home() {
 
   return (
     <HomeContainer ref={sliderRef} className="keen-slider">
-      <ProductCard href="#" className="keen-slider__slide">
-        <Image src={igniteLab} width={520} height={480} alt="" />
-        <footer>
-          <strong>Camiseta Ignite Lab</strong>
-          <span>R$ 89,99</span>
-        </footer>
-      </ProductCard>
-
-      <ProductCard href="#" className="keen-slider__slide">
-        <Image src={igniterAbord} width={520} height={480} alt="" />
-        <footer>
-          <strong>Camiseta Igniter Aboard</strong>
-          <span>R$ 79,99</span>
-        </footer>
-      </ProductCard>
-
-      <ProductCard href="#" className="keen-slider__slide">
-        <Image src={maratonaExplorer} width={520} height={480} alt="" />
-        <footer>
-          <strong>Camiseta Maratona Explorer</strong>
-          <span>R$ 69,99</span>
-        </footer>
-      </ProductCard>
-
-      <ProductCard href="#" className="keen-slider__slide">
-        <Image src={maratonaExplorer} width={520} height={480} alt="" />
-        <footer>
-          <strong>Camiseta Maratona Explorer</strong>
-          <span>R$ 69,99</span>
-        </footer>
-      </ProductCard>
+      {products.map((product) => {
+        return (
+          <ProductCard key={product.id} href="#" className="keen-slider__slide">
+            <Image src={product.imageUrl} width={520} height={480} alt="" />
+            <footer>
+              <strong>{product.name}</strong>
+              <span>{product.price}</span>
+            </footer>
+          </ProductCard>
+        )
+      })}
     </HomeContainer>
   )
+}
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const response = await stripe.products.list({
+    expand: ['data.default_price'],
+  })
+
+  const products = response.data.map((product) => {
+    const price = product.default_price as Stripe.Price
+
+    return {
+      id: product.id,
+      name: product.name,
+      description: product.description,
+      imageUrl: product.images[0],
+      price: price.unit_amount / 100,
+    }
+  })
+
+  return {
+    props: {
+      products,
+    },
+  }
 }

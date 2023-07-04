53ba608e617b28356a816f3f3e74aa6518f5ff06
rafaelreisramos <rrramos@gmail.com>
2023-07-04 09:31:01 -0300

    feat: product page html/css

--- projeto-04__ignite-shop/src/pages/product/[id].tsx
+++ projeto-04__ignite-shop/src/pages/product/[id].tsx
@@ -1,7 +1,29 @@
 import { useRouter } from 'next/router'
+import {
+  ImageContainer,
+  ProductContainer,
+  ProductDetails,
+} from '../../styles/pages/product'
 
-export default function ProductDetails() {
+export default function Product() {
   const { query } = useRouter()
 
-  return <h1>Product: {JSON.stringify(query)}</h1>
+  return (
+    <ProductContainer>
+      <ImageContainer></ImageContainer>
+      <ProductDetails>
+        <h1>Camiseta X</h1>
+
+        <span>R$ 79,90</span>
+
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius aliquid
+          fugit ab dicta ullam illum? Quod, porro. Sit cumque voluptatem et?
+          Maxime ea quia at voluptas voluptates laborum sunt voluptatum.
+        </p>
+
+        <button>Comprar agora</button>
+      </ProductDetails>
+    </ProductContainer>
+  )
 }
---
+++ projeto-04__ignite-shop/src/styles/pages/product.ts
@@ -0,0 +1,70 @@
+import { styled } from '..'
+
+export const ProductContainer = styled('main', {
+  display: 'grid',
+  gridTemplateColumns: '1fr 1fr',
+  gap: '4.5rem',
+  alignItems: 'stretch',
+
+  width: 'min(100%, 1180px)',
+  margin: '0 auto',
+})
+
+export const ImageContainer = styled('div', {
+  borderRadius: 8,
+  background: 'linear-gradient(180deg, #1ea483 0%, #7465d4 100%)',
+  padding: '5.5rem 1.75rem',
+  maxWidth: 'max(576, 100%)',
+  height: 656,
+
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+
+  img: {
+    objectFit: 'cover',
+  },
+})
+
+export const ProductDetails = styled('div', {
+  display: 'flex',
+  flexDirection: 'column',
+
+  h1: {
+    color: '$gray300',
+    fontWeight: 700,
+    fontSize: '$2xl',
+  },
+
+  span: {
+    display: 'inline-block',
+    color: '$green300',
+    fontSize: '$2xl',
+    marginTop: '1rem',
+  },
+
+  p: {
+    color: '$gray300',
+    fontSize: '$md',
+    lineHeight: 1.6,
+    marginTop: '2.5rem',
+  },
+
+  button: {
+    border: 0,
+    cursor: 'pointer',
+    marginTop: 'auto',
+
+    padding: '1.25rem 2rem',
+    background: '$green500',
+    borderRadius: 8,
+
+    fontWeight: 700,
+    fontSize: '$md',
+    color: '$white',
+
+    '&:hover': {
+      background: '$green300',
+    },
+  },
+})

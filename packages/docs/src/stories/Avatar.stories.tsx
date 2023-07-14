6468ae4ff662694a955c1fb414a04e8837ba6d90
rafaelreisramos <rrramos@gmail.com>
2023-07-14 14:45:29 -0300

    chore: add `Avatar` component and story

---
+++ packages/docs/src/stories/Avatar.stories.tsx
@@ -0,0 +1,25 @@
+import type { Meta, StoryObj } from '@storybook/react'
+
+import { Avatar, AvatarProps } from '@ignite-ui/react'
+
+const meta: Meta<AvatarProps> = {
+  title: 'Data display/Avatar',
+  component: Avatar,
+  tags: ['autodocs'],
+  args: {
+    src: 'https://github.com/rafaelreisramos.png',
+    alt: 'Rafael Reis Ramos',
+  },
+} as Meta
+
+export default meta
+
+type Story = StoryObj<AvatarProps>
+
+export const Primary: Story = {}
+
+export const WithFallback: Story = {
+  args: {
+    src: undefined,
+  },
+}

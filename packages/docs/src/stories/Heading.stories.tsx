a8f11371791576678988cbf69234cb8d7b78e2b7
rafaelreisramos <rrramos@gmail.com>
2023-07-14 14:24:39 -0300

    chore: add `Heading` component and story

---
+++ packages/docs/src/stories/Heading.stories.tsx
@@ -0,0 +1,33 @@
+import type { Meta, StoryObj } from '@storybook/react'
+
+import { Heading, HeadingProps } from '@ignite-ui/react'
+
+const meta: Meta<HeadingProps> = {
+  title: 'Typography/Heading',
+  component: Heading,
+  tags: ['autodocs'],
+  args: {
+    children: 'Custom title',
+  },
+} as Meta
+
+export default meta
+
+type Story = StoryObj<HeadingProps>
+
+export const Primary: Story = {}
+
+export const CustomTag: Story = {
+  args: {
+    children: 'H1 Heading',
+    as: 'h1',
+  },
+  parameters: {
+    docs: {
+      description: {
+        story:
+          'Heading will be `h2` by default. You can add an `as` prop with your custom tag.',
+      },
+    },
+  },
+}

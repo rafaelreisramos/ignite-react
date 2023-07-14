be9bb8a66d7884d4f07bb2a16c06f1bbcdb6ea2f
rafaelreisramos <rrramos@gmail.com>
2023-07-14 13:48:43 -0300

    chore: add `Text` component and story

---
+++ packages/docs/src/stories/Text.stories.tsx
@@ -0,0 +1,26 @@
+import type { Meta, StoryObj } from '@storybook/react'
+
+import { Text, TextProps } from '@ignite-ui/react'
+
+const meta: Meta<TextProps> = {
+  title: 'Typography/Text',
+  component: Text,
+  args: {
+    children:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt, magni. Dolor laborum fugit labore aliquam. Fuga cumque quos, reprehenderit impedit sint voluptatum quis corporis deserunt, rerum culpa architecto provident nemo.',
+  },
+} as Meta
+
+export default meta
+
+type Story = StoryObj<TextProps>
+
+export const Primary: Story = {}
+
+export const CustomTag: Story = {
+  args: {
+    children:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt, magni. Dolor laborum fugit labore aliquam. Fuga cumque quos, reprehenderit impedit sint voluptatum quis corporis deserunt, rerum culpa architecto provident nemo.',
+    as: 'strong',
+  },
+}

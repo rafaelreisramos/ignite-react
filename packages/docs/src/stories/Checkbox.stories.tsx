2d4c425e9e7b89b6d864ab6496e868150f2f5ea0
rafaelreisramos <rrramos@gmail.com>
2023-07-17 09:15:25 -0300

    chore: add `Checkbox` component and story

---
+++ packages/docs/src/stories/Checkbox.stories.tsx
@@ -0,0 +1,25 @@
+import type { Meta, StoryObj } from '@storybook/react'
+
+import { Box, Text, Checkbox, CheckboxProps } from '@ignite-ui/react'
+
+const meta: Meta<CheckboxProps> = {
+  title: 'Form/Checkbox',
+  component: Checkbox,
+  tags: ['autodocs'],
+  decorators: [
+    (Story) => {
+      return (
+        <Box as="label" css={{ display: 'flex', gap: '$2' }}>
+          {Story()}
+          <Text size="sm">Accept terms of use</Text>
+        </Box>
+      )
+    },
+  ],
+} as Meta
+
+export default meta
+
+type Story = StoryObj<CheckboxProps>
+
+export const Primary: Story = {}
---
+++ packages/react/src/components/Checkbox/styles.ts
@@ -0,0 +1,61 @@
+import * as Checkbox from '@radix-ui/react-checkbox'
+
+import { keyframes, styled } from '../../styles'
+
+export const CheckboxContainer = styled(Checkbox.Root, {
+  all: 'unset',
+  lineHeight: 0,
+  cursor: 'pointer',
+
+  width: '$6',
+  height: '$6',
+  overflow: 'hidden',
+  backgroundColor: '$gray900',
+  borderRadius: '$xs',
+  border: '2px solid $gray900',
+
+  boxSizing: 'border-box',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+
+  '&[data-state="checked"]': {
+    backgroundColor: '$ignite300',
+  },
+
+  '&:focus': {
+    border: '2px solid $ignite300',
+  },
+})
+
+const slideIn = keyframes({
+  from: {
+    transform: 'translateY(-100%)',
+  },
+  to: {
+    transform: 'translateY(0)',
+  },
+})
+
+const slideOut = keyframes({
+  from: {
+    transform: 'translateY(0)',
+  },
+  to: {
+    transform: 'translateY(-100%)',
+  },
+})
+
+export const CheckboxIndicator = styled(Checkbox.Indicator, {
+  width: '$4',
+  height: '$4',
+  color: '$white',
+
+  '&[data-state="checked"]': {
+    animation: `${slideIn} 200ms ease-out`,
+  },
+
+  '&[data-state="unchecked"]': {
+    animation: `${slideOut} 200ms ease-out`,
+  },
+})
